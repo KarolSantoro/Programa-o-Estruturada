dcdf52c7bb00f44b48aa8bab24459044555da097
KarolSantoro <166721482+KarolSantoro@users.noreply.github.com>
2024-04-11 11:18:28 -0300

    Add files via upload

---
+++ Calculo de um retangulo.py
@@ -0,0 +1,6 @@
+base = float(input ("Digite a largura do retângulo:"))
+altura = float(input ("Digite a altura do retângulo:"))
+perimetro = 2 * (base + altura)
+area = base * altura
+print ("O perímetro do retângulo é:" , (perimetro))
+print ("A área do retângulo é:" , (area))
