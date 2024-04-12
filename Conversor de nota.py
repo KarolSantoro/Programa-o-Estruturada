2636fdf97fae02db1b3342df2efc7a04d1e2fd8f
KarolSantoro <166721482+KarolSantoro@users.noreply.github.com>
2024-04-12 08:20:08 -0300

    Add files via upload

---
+++ Conversor de nota.py
@@ -0,0 +1,11 @@
+nota = float(input("Digite a nota:"))
+if nota >=+9:
+    print("A")
+elif nota >=+7.5:
+    print("B")
+elif nota >=6:
+    print("C")
+elif nota >=4:
+    print("D")
+else:
+    print ("F")
