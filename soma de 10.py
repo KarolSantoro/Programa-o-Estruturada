60377027d0db8f74890c5b0fb8abb060fea97d22
KarolSantoro <166721482+KarolSantoro@users.noreply.github.com>
2024-06-06 10:29:53 -0300

    Add files via upload

---
+++ soma de 10.py
@@ -0,0 +1,8 @@
+#10 nº inteiros e retornar para a soma dos 10
+soma = 0
+contador = 0
+while contador < 10:
+    num = int(input("Digite um número:"))
+    soma = num + soma
+    contador = contador + 1
+print ("A soma é:" , soma)
